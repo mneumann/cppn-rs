c95d3768b5b4563aab095098d8a352b9dbc07af7
Michael Neumann <mneumann@ntecs.de>
2016-02-15 14:35:35 +0100

    Make the output type of ActivationFunction a type parameter.

--- src/bipolar/mod.rs
+++ src/bipolar/mod.rs
@@ -1,11 +1,7 @@
 use self::closed01bipolar::Closed01Bipolar;
+use super::ActivationFunction;
 
 pub mod closed01bipolar;
-
-pub trait ActivationFunction {
-    fn formula() -> &'static str;
-    fn calculate(x: f64) -> Closed01Bipolar<f64>;
-}
 
 pub struct Linear;
 pub struct Gaussian;
@@ -13,41 +9,49 @@ pub struct Sigmoid;
 pub struct Sine;
 
 impl ActivationFunction for Linear {
+    type Output = Closed01Bipolar<f64>;
+
     fn formula() -> &'static str {
         "y = max(-1.0, min(1.0, x))"
     }
 
-    fn calculate(x: f64) -> Closed01Bipolar<f64> {
+    fn calculate(x: f64) -> Self::Output {
         Closed01Bipolar::new_clipped(x)
     }
 }
 
 impl ActivationFunction for Gaussian {
+    type Output = Closed01Bipolar<f64>;
+
     fn formula() -> &'static str {
         "y = 2.0 * exp(-(x * 2.5)^2.0) - 1.0"
     }
 
-    fn calculate(x: f64) -> Closed01Bipolar<f64> {
+    fn calculate(x: f64) -> Self::Output {
         Closed01Bipolar::new(2.0 * (-(x * 2.5).powi(2)).exp() - 1.0)
     }
 }
 
 impl ActivationFunction for Sigmoid {
+    type Output = Closed01Bipolar<f64>;
+
     fn formula() -> &'static str {
         "y = 2.0 / (1.0 + exp(-4.9 * x)) - 1.0"
     }
 
-    fn calculate(x: f64) -> Closed01Bipolar<f64> {
+    fn calculate(x: f64) -> Self::Output {
         Closed01Bipolar::new((2.0 / (1.0 + (-4.9 * x).exp())) - 1.0)
     }
 }
 
 impl ActivationFunction for Sine {
+    type Output = Closed01Bipolar<f64>;
+
     fn formula() -> &'static str {
         "y = sin(2.0 * x)"
     }
 
-    fn calculate(x: f64) -> Closed01Bipolar<f64> {
+    fn calculate(x: f64) -> Self::Output {
         Closed01Bipolar::new(2.0 * x.sin())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
+use std::convert::Into;
+
+pub trait ActivationFunction {
+    type Output: Into<f64>;
+    fn formula() -> &'static str;
+    fn calculate(x: f64) -> Self::Output;
+}
+
 pub mod bipolar;
