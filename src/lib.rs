bb635aa91e3c1bc4f57abd818d1794d0483e16f2
Michael Neumann <mneumann@ntecs.de>
2016-02-16 15:19:35 +0100

    Add Identity function

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,17 @@ pub trait ActivationFunction {
     fn calculate(x: f64) -> f64;
 }
 
+pub struct Identity;
+
+impl ActivationFunction for Identity {
+    fn formula() -> &'static str {
+        "y = x"
+    }
+
+    fn calculate(x: f64) -> f64 {
+        x
+    }
+}
+
 pub mod bipolar;
 pub mod closed01bipolar;
