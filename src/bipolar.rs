584b2a113700b7a71983af275356e5045abd7fa7
Michael Neumann <mneumann@ntecs.de>
2016-02-16 15:27:47 +0100

    Let method of trait ActivationFunction take &self. This allows that we can make use of trait objects. Also add a simple test case for Linear.

--- src/bipolar.rs
+++ src/bipolar.rs
@@ -23,41 +23,52 @@ pub struct Sigmoid;
 pub struct Sine;
 
 impl ActivationFunction for Linear {
-    fn formula() -> &'static str {
+    fn formula(&self) -> &'static str {
         "y = max(-1.0, min(1.0, x))"
     }
 
-    fn calculate(x: f64) -> f64 {
+    fn calculate(&self, x: f64) -> f64 {
         bipolar_debug_check(bipolar_clip(x))
     }
 }
 
 impl ActivationFunction for Gaussian {
-    fn formula() -> &'static str {
+    fn formula(&self) -> &'static str {
         "y = 2.0 * exp(-(x * 2.5)^2.0) - 1.0"
     }
 
-    fn calculate(x: f64) -> f64 {
+    fn calculate(&self, x: f64) -> f64 {
         bipolar_debug_check(2.0 * (-(x * 2.5).powi(2)).exp() - 1.0)
     }
 }
 
 impl ActivationFunction for Sigmoid {
-    fn formula() -> &'static str {
+    fn formula(&self) -> &'static str {
         "y = 2.0 / (1.0 + exp(-4.9 * x)) - 1.0"
     }
 
-    fn calculate(x: f64) -> f64 {
+    fn calculate(&self, x: f64) -> f64 {
         bipolar_debug_check((2.0 / (1.0 + (-4.9 * x).exp())) - 1.0)
     }
 }
 
 impl ActivationFunction for Sine {
-    fn formula() -> &'static str {
+    fn formula(&self) -> &'static str {
         "y = sin(2.0 * x)"
     }
 
-    fn calculate(x: f64) -> f64 {
+    fn calculate(&self, x: f64) -> f64 {
         bipolar_debug_check((2.0 * x).sin())
     }
+}
+
+#[test]
+fn test_linear() {
+    assert_eq!(0.0, Linear.calculate(0.0));
+    assert_eq!(1.0, Linear.calculate(1.0));
+    assert_eq!(-1.0, Linear.calculate(-1.0));
+    assert_eq!(0.5, Linear.calculate(0.5));
+    assert_eq!(-0.5, Linear.calculate(-0.5));
+    assert_eq!(1.0, Linear.calculate(1.1));
+    assert_eq!(-1.0, Linear.calculate(-1.1));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 pub trait ActivationFunction {
-    fn formula() -> &'static str;
-    fn calculate(x: f64) -> f64;
+    fn formula(&self) -> &'static str;
+    fn calculate(&self, x: f64) -> f64;
 }
 
 pub struct Identity;
 
 impl ActivationFunction for Identity {
-    fn formula() -> &'static str {
+    fn formula(&self) -> &'static str {
         "y = x"
     }
 
-    fn calculate(x: f64) -> f64 {
+    fn calculate(&self, x: f64) -> f64 {
         x
     }
 }
