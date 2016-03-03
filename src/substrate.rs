2001336a57dbdcd1637164ec5b6fd8fda8fe8922
Michael Neumann <mneumann@ntecs.de>
2016-03-03 19:42:23 +0100

    Use an internal iterator, as it's much easier.

--- src/substrate.rs
+++ src/substrate.rs
@@ -20,20 +20,6 @@ pub struct Substrate<P, T>
     nodes: Vec<Node<P, T>>,
 }
 
-pub struct LinkIterator<'a,
-                        N: CppnNodeType + 'a,
-                        L: Copy + Debug + Send + Sized + Into<f64> + 'a,
-                        EXTID: Copy + Debug + Send + Sized + Ord + 'a,
-                        P: Position + 'a,
-                        T: NodeType + 'a>
-{
-    nodes: &'a [Node<P, T>],
-    cppn: &'a mut Cppn<'a, N, L, EXTID>,
-    inner: usize,
-    outer: usize,
-    max_distance: Option<f64>,
-}
-
 #[derive(Copy, Clone)]
 pub struct Link<'a, P, T>
     where P: Position + 'a,
@@ -45,70 +31,6 @@ pub struct Link<'a, P, T>
     pub target_idx: usize,
     pub weight: f64,
     pub distance: f64,
-}
-
-impl<'a, N: CppnNodeType + 'a,
-   L: Copy + Debug + Send + Sized + Into<f64> + 'a,
-    EXTID: Copy + Debug + Send + Sized + Ord + 'a,
-P: Position + 'a, T: NodeType + 'a> Iterator for LinkIterator<'a, N, L, EXTID, P, T> {
-    type Item = Link<'a, P, T>;
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if self.inner >= self.nodes.len() {
-                self.inner = 0;
-                self.outer += 1;
-            }
-            if self.outer >= self.nodes.len() {
-                return None;
-            }
-            if self.inner == self.outer {
-                self.inner += 1;
-                continue;
-            }
-
-            assert!(self.inner < self.nodes.len() && self.outer < self.nodes.len() &&
-                    self.inner != self.outer);
-            let source = &self.nodes[self.inner];
-            let target = &self.nodes[self.outer];
-
-// Reject invalid connections.
-
-            if !source.node_type.accept_outgoing_links() || !target.node_type.accept_incoming_links() {
-                    self.inner += 1;
-                    continue;
-            }
-
-            let distance = source.position.distance(&target.position);
-
-// reject a pair of nodes based on `max_distance`.
-
-            if let Some(max_d) = self.max_distance {
-                if distance > max_d {
-                    self.inner += 1;
-                    continue;
-                }
-            }
-
-// Calculate the weight between source and target using the CPPN.
-
-            let distance_between: &[_] = &[distance];
-            let inputs_to_cppn = [source.position.coords(), target.position.coords(), distance_between];
-            let outputs_from_cppn = self.cppn.calculate(&inputs_to_cppn);
-            assert!(outputs_from_cppn.len() == 1);
-            let weight = outputs_from_cppn[0];
-
-            let link = Link {
-                source: source,
-                target: target,
-                source_idx: self.inner,
-                target_idx: self.outer,
-                weight: weight,
-                distance: distance,
-            };
-            self.inner += 1;
-            return Some(link);
-        }
-    }
 }
 
 impl<P, T> Substrate<P, T>
@@ -131,20 +53,57 @@ impl<P, T> Substrate<P, T>
     }
 
     /// Iterate over all produced links of Cppn.
-    pub fn iter_links<'a, N, L, EXTID>(&'a self,
-                                       cppn: &'a mut Cppn<'a, N, L, EXTID>,
-                                       max_distance: Option<f64>)
-                                       -> LinkIterator<'a, N, L, EXTID, P, T>
+
+    pub fn each_link<'a, N, L, EXTID, F>(&'a self,
+                                         cppn: &'a mut Cppn<'a, N, L, EXTID>,
+                                         max_distance: Option<f64>,
+                                         callback: &mut F)
         where N: CppnNodeType,
               L: Copy + Debug + Send + Sized + Into<f64> + 'a,
-              EXTID: Copy + Debug + Send + Sized + Ord + 'a
+              EXTID: Copy + Debug + Send + Sized + Ord + 'a,
+              F: FnMut(Link<'a, P, T>)
     {
-        LinkIterator {
-            nodes: &self.nodes,
-            cppn: cppn,
-            inner: 0,
-            outer: 0,
-            max_distance: max_distance,
+        for (source_idx, source) in self.nodes.iter().enumerate() {
+            // Reject invalid connections.
+            if !source.node_type.accept_outgoing_links() {
+                continue;
+            }
+
+            for (target_idx, target) in self.nodes.iter().enumerate() {
+                // Reject invalid connections.
+                if !target.node_type.accept_incoming_links() {
+                    continue;
+                }
+
+                let distance = source.position.distance(&target.position);
+
+                // reject a pair of nodes based on `max_distance`.
+                if let Some(max_d) = max_distance {
+                    if distance > max_d {
+                        continue;
+                    }
+                }
+
+                // Calculate the weight between source and target using the CPPN.
+
+                let distance_between: &[_] = &[distance];
+                let inputs_to_cppn = [source.position.coords(),
+                                      target.position.coords(),
+                                      distance_between];
+                let outputs_from_cppn = cppn.calculate(&inputs_to_cppn);
+                assert!(outputs_from_cppn.len() == 1);
+                let weight = outputs_from_cppn[0];
+
+                let link = Link {
+                    source: source,
+                    target: target,
+                    source_idx: source_idx,
+                    target_idx: target_idx,
+                    weight: weight,
+                    distance: distance,
+                };
+                callback(link);
+            }
         }
     }
 }
